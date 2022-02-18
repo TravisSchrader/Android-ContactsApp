66c3248db6faacce086637c915b755e01df532c1
TravisSchrader <travis.ts.schrader@gmail.com>
2022-02-18 13:25:47 -0800

    Cleaned up some comments, checking if I am pushing from my personal git as well

--- ContactsApp/app/src/main/java/com/example/ContactsApp/add_contact_info.kt
+++ ContactsApp/app/src/main/java/com/example/ContactsApp/add_contact_info.kt
@@ -8,17 +8,11 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// arguments passed to this fragment which let me know if this fragment is being used to edit a contact or not
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 private const val ARG_PARAM3 = "param3"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [add_contact_info.newInstance] factory method to
- * create an instance of this fragment.
- */
 class add_contact_info : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var name: String? = null
@@ -52,9 +46,10 @@ class add_contact_info : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-//        dialog?.setTitle("Enter New Contact: ")
+
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         dialog?.window?.setLayout(100,100)
+
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_add_contact_info, container, false)
 
@@ -109,15 +104,6 @@ class add_contact_info : DialogFragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment add_contact_info.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             add_contact_info().apply {
